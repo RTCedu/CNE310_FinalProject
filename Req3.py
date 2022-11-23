21bf600ce80564ed98208c0e2dae07060b40a024
BASHAR7A7 <117067788+BASHAR7A7@users.noreply.github.com>
2022-11-23 13:49:46 -0800

    Create Req3

---
+++ Req3.py
@@ -0,0 +1,8 @@
+@app.route("/add")
+def admin():
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT categoryId, name FROM categories")
+        categories = cur.fetchall()
+    conn.close()
+    return render_template('add.html', categories=categories)
