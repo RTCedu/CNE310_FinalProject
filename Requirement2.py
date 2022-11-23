93aa39ae832bbdf3d71f26da465121beef331135
Ceceskii <97866655+Ceceskii@users.noreply.github.com>
2022-11-23 00:11:15 -0800

    Create Requirement2

---
+++ Requirement2.py
@@ -0,0 +1,19 @@
+@app.route("/loginForm")
+def login_form():
+    # Uncomment to enable logging in and registration
+    #if 'email' in session:
+        return redirect(url_for('root'))
+    #else:
+    #    return render_template('login.html', error='')
+
+@app.route("/login", methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if is_valid(email, password):
+            session['email'] = email
+            return redirect(url_for('root'))
+        else:
+            error = 'Invalid UserId / Password'
+            return render_template('login.html', error=error)
